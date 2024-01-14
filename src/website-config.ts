a952a6bd4fa32f6e72505934ee8286e49abe24a3
Yu O Jun <yoj9168@gmail.com>
2024-01-14 14:48:45 +0900

    fix: title

--- src/website-config.ts
+++ src/website-config.ts
@@ -54,7 +54,7 @@ export type WebsiteConfig = {
 };
 
 const config: WebsiteConfig = {
-  title: 'Sky Castle',
+  title: '남해 스카이캐슬',
   description: '당신의 삶에 감동을 선물하는 남해 스카이캐슬! 자신있게 선보입니다!',
   coverImage: 'img/home.jpg',
   logo: 'img/logo.jpg',
